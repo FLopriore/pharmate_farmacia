a8a2040a9c070f12706edd7c7c06e5ebe73d8e76
FrancescoDesiderato <desiderato.francescosalvatore@gmail.com>
2023-11-14 20:19:18 +0100

    Added Custom Pharmate Icon

---
+++ lib/widgets/custom_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class CustomIcon extends StatelessWidget {
+  final String icon;
+  final double height;
+  final double width;
+
+  const CustomIcon(
+      {Key? key, required this.icon, this.height = 60, this.width = 60})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      image: AssetImage(icon),
+      color: null,
+      height: height,
+      width: width,
+    );
+  }
+}
