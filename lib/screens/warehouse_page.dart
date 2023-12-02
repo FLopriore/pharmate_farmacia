82c666e6c834687b3ba25aec4d1f859c85d98048
FLopriore <115494520+FLopriore@users.noreply.github.com>
2023-12-02 12:29:47 +0100

    Reformat code

--- lib/screens/warehouse_page.dart
+++ lib/screens/warehouse_page.dart
@@ -9,56 +9,80 @@ class WarehousePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
+      body: Column(children: [
         const SizedBox(height: 20),
         const Align(
-            alignment: Alignment.center,
-            child: Text(
-              "Magazzino",
-              style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 60,
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.center,
+          alignment: Alignment.center,
+          child: Text(
+            "Magazzino",
+            style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 60,
+              color: Colors.black,
             ),
+            textAlign: TextAlign.center,
+          ),
         ),
         Align(
           alignment: Alignment.center,
           child: Row(
             children: [
-              const Expanded(child: SizedBox(height: 50,child: CustomSearchBar(),),),
+              const Expanded(
+                child: SizedBox(
+                  height: 50,
+                  child: CustomSearchBar(),
+                ),
+              ),
               Directionality(
                 textDirection: TextDirection.rtl,
                 child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xff023D74),
-                    foregroundColor: Colors.white,
-                    fixedSize: Size.fromHeight(45),
-                  ),
-                  onPressed: (){}, 
-                  icon:const Icon(Icons.search), 
-                  label: const Text("Cerca ",style: TextStyle(fontWeight: FontWeight.normal,fontSize: 15),)),), //TODO: Filter the drugs using a query
-              const SizedBox(width: 10,),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff023D74),
+                      foregroundColor: Colors.white,
+                      fixedSize: const Size.fromHeight(45),
+                    ),
+                    onPressed: () {},
+                    icon: const Icon(Icons.search),
+                    label: const Text(
+                      "Cerca ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 15),
+                    )),
+              ), //TODO: Filter the drugs using a query
+              const SizedBox(
+                width: 10,
+              ),
               Directionality(
-                textDirection: TextDirection.rtl, 
+                textDirection: TextDirection.rtl,
                 child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xff023D74),
-                    foregroundColor: Colors.white,
-                    fixedSize: Size.fromHeight(45),
-                  ),
-                  onPressed: () => showDialog(context: context, builder: (BuildContext context) => DialogAddStock()),
-                  icon:const Icon(Icons.add), 
-                  label: const Text("Aggiungi Giacenza ",style: TextStyle(fontWeight: FontWeight.normal,fontSize: 15),)),), //TODO: Pop up to insert a new drug to warehouse
-              const SizedBox(width: 20,)
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff023D74),
+                      foregroundColor: Colors.white,
+                      fixedSize: const Size.fromHeight(45),
+                    ),
+                    onPressed: () => showDialog(
+                        context: context,
+                        builder: (BuildContext context) =>
+                            const DialogAddStock()),
+                    icon: const Icon(Icons.add),
+                    label: const Text(
+                      "Aggiungi Giacenza ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 15),
+                    )),
+              ), //TODO: Pop up to insert a new drug to warehouse
+              const SizedBox(
+                width: 20,
+              )
             ],
           ),
-        ), 
-         const Expanded(child:Padding(padding: EdgeInsets.symmetric(vertical: 20),child: RoundedBackgroundRectangle(child: Placeholder()),))
-        ]
-      ),
+        ),
+        const Expanded(
+            child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: RoundedBackgroundRectangle(child: Placeholder()),
+        ))
+      ]),
     );
   }
 }
